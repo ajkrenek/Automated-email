bc33aa997c3d392ce638fba98a0ca1289907aca1
ajkrenek <98238097+ajkrenek@users.noreply.github.com>
2022-07-06 21:34:52 -0500

    Update automail.py

--- automail.py
+++ automail.py
@@ -1,81 +1,170 @@
-import smtplib, ssl, random, schedule, time
+import smtplib, ssl, random, schedule, time, praw, datetime, re
 import urllib.request
-import os
 from email.message import EmailMessage
 from email.utils import make_msgid
 from bs4 import BeautifulSoup
 
+    #global variables
+now = datetime.datetime.now()
+today_date = datetime.date.today()
+first_date=date(2020, 8, 17)
+ad = 17
+cy = now.year
+cm = now.month
+special_date = datetime.date(cy, cm, ad)
 
-def example_quote():
-    with open('example quotes.txt', 'r') as f:
+years_since = ((special_date - first_date).days)/365
+digits = math.modf(years_since)
+years = str(digits[1])
+year = years[0]
+months = str(digits[0])
+month = months[2]
+
+reddit = praw.Reddit(client_id="", client_secret='',
+
+                 password='', user_agent='',
+
+                 username='')
+
+def reddit_quote():
+
+    with open('reddit.txt', 'r') as f:
         read = f.read()
         array = read.split('\n')
-        quote = random.choice(array)
-    return quote
+        link = random.choice(array)
 
-def example_url():
-    with open('example.txt', 'r') as f:
-        read = f.read()
-        array = read.split('\n')
-        tquote = random.choice(array)
-#opens random url from text file, scrapes the text from the webpage between <p> tags and removes the <p> tags
-    html = urllib.request.urlopen(tquote).read()
+    reddit_url = link
+    another_list = []
+
+
+    submissions = reddit.submission(url=reddit_url)
+    for top_level_comment in submissions.comments:
+        another_list.append(top_level_comment.body)
+
+    reddit_comment = random.choice(another_list)
+    for item in reddit_comment:
+        if item == '[removed]':
+            continue
+        if item == '[deleted]':
+            continue
+        if item == ' ':
+            new_reddit_comment = reddit_comment
+            break
+    return new_reddit_comment
+
+def tumblr_url():
+    tumblr_url = 'https://www.tumblr.com/search/love%20quotes?t=1'
+    html = urllib.request.urlopen(tumblr_url).read()
     soup = BeautifulSoup(html, 'html.parser')
-    p_tag = soup.p
-    text_p_tag = p_tag.contents[0]
-    # combines the text from the webpage and the link to the post
-    text_url = text_p_tag + '\n' + tquote
-    return text_url
+    some_list = []
+    for post in soup.find_all('a', href=True):
+        some_list.append(post['href'])
 
-def picture():
-    image = random.choice(os.listdir("file name"))
-    return image
+
+    post_url = re.findall(r'''
+    #structure of post url : https://somename.tumblr.com/post/12094812094814/some-post-name
+    https?://                      # https part
+    (?:[-\w.]|(?:%[\da-fA-F]{2}))+ # name.tumblr part
+    /                              # forward slash
+    \w+                            #'post'
+    /                              # forward slash
+    \d+                            # the digits
+    /                              # forward slash
+    [\w-]*                         # some-post-name
+    ''',str(some_list), re.VERBOSE)
+
+    post = random.choice(post_url)
+    return post
+
+def reddit_picture_url():
+
+    url_list = ""
+    sub = "frogs"
+    subreddit = reddit.subreddit(sub)
+    new_subreddit = subreddit.new(limit=20)
+
+    for submission in new_subreddit:
+        picture_url = str(submission.url)
+        if picture_url.endswith('jpg') or picture_url.endswith('jpeg') or picture_url.endswith('png'):
+            url_list += picture_url +'\n'
+
+    url_link = url_list.split()
+    random_url = random.choice(url_link)
+
+    return random_url
 
 #setting up email/message
+
+    #global email variables
+
+email = ""
+password = ""
+port = 465 #gmail port
+
 def mail():
-    port = 465 #gmail server port
-#calls the returned values
-    example_pic = picutre()
-    exampleQ = example_quote()
-    exampleU = example_url()
+    Quote = reddit_quote()
+    Url = tumblr_url()
+    Picture = reddit_picture_url()
 
     msg = EmailMessage()
 
-    msg["Subject"] = "Automated Test"
-    msg["From"] = "sender@email.com"
-    msg["To"] = "receiver@email.com"
-    
-    picture_cid = make_msgid()
+    msg["Subject"] = ""
+    msg["From"] = ""
+    msg["To"] = ""
 
     msg.add_alternative("""\
     <html>
+        <head> <h1>  </h1> </head>
         <body>
-            <p> {exampleQ} </p>
-            <p> {exampleU} </p>
-            <p> Insert your picture
-                <a href="https://i.imgur.com/XqoqnyH.jpg"> </a> #can insert with url link too
-            </p>
-            <img src = "cid:{picture_cid}" />
+            <p> {Quote} </p>
+            <p> {Url} </p>
+            <img src = {Picture}
+            width="400" height="400"/> 
         </body>
     </html>
-""".format(exampleQ=exampleQ, exampleU=exampleU, picture_cid=picture_cid[1:-1]), subtype = 'html')
+""".format(Quote=Quote, Url=Url, Picture=Picture), subtype = 'html')
 
-    with open("file name/" + example_pic, 'rb') as img:
-        msg.get_payload()[0].add_related(img.read(), 'image', 'jpeg', cid=picture_cid)
-
-    email = "sender@email.com"
-    password = ""
-
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    with smtplib.SMTP_SSL("smtp.gmail.com", port) as server:
         server.login(email, password)
         server.send_message(msg)
         server.quit()
 
-#schedule email daily
-schedule.every().day.at("15:00").do(mail)
+def special_mail():
+    loveQ = love_quote()
+    loveU = love_url()
+    frogU = frog_picture_url()
+
+    msg = EmailMessage()
+
+    msg["Subject"] = ""
+    msg["From"] = ""
+    msg["To"] = ""
+
+    msg.add_alternative("""\
+    <html>
+        <head> <h1> Special Day </h1> </head>
+        <h2> Happy {years} years and {months} months!!</h2>
+        <body>
+            <p> {Quote} </p>
+            <p> {Url} </p>
+            <img src = {Picture}
+            width="400" height="400"/> 
+        </body>
+    </html>
+""".format(Quote=Quote, Url=Url, Picture=Picture, years=year, months=month), subtype = 'html')
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port) as server:
+        server.login(email, password)
+        server.send_message(msg)
+        server.quit()
+
+if today_date == special_date:
+    schedule.every().day.at("20:00").do(special_mail)
+else:
+    schedule.every().day.at("20:00").do(mail)
 while True:
     schedule.run_pending()
     time.sleep(1)
+
+
 
