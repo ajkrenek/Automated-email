0b3932835b9a99b6ef024dfa8a3b0326c9d997d0
ajkrenek <98238097+ajkrenek@users.noreply.github.com>
2023-10-01 11:32:37 -0500

    Update automail.py
    Cleaned up and removed functions to improve speed and readability

--- automail.py
+++ automail.py
@@ -1,10 +1,16 @@
-import datetime, math
-import json, random, pytumblr, praw
+import math
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+import json, random
+import pytumblr2
+import praw
 import schedule, time
 import urllib.request
 import smtplib
 
 from bs4 import BeautifulSoup
+import requests
 from email.message import EmailMessage
 from email.utils import make_msgid
 from dataclasses import dataclass
@@ -26,60 +32,77 @@ oauth_secret=config_data['OAUTH_SECRET']
 client = pytumblr.TumblrRestClient(client_key, client_secret, oauth_token, oauth_secret)
 
 
-ahora = datetime.datetime.now()
-today_date = datetime.date.today()
+ahora = datetime.now()
+current_day = ahora.day
 current_year = ahora.year
 current_month = ahora.month
 
-def ann_date(ann_year: int, ann_month:int, ann_day: int) -> list[int]:
+def tumblr_post(quote_query):
+    search_url = 'https://www.tumblr.com/search/{}?t=1'.format(random.choice(quote_query).replace(" ", "%20"))
+
+    random_a_tag = random.choice(BeautifulSoup(urllib.request.urlopen(search_url).read(), 'html.parser').find_all("a", {"class": "BSUG4"}))
     
-    first_date = datetime.date(ann_year, ann_month, ann_day)
-    anniversary_date = datetime.date(current_year, current_month, ann_day)
-    years_since = ((anniversary_date - first_date).days)/365          #converts number of days into years
-    rounded = round(years_since,5)                            #rounds to 5 decimal place
-    digits = math.modf(rounded)                               #splits the decimal into two: [decimal places, whole numbers]
-    years = math.floor(digits[1])                             #takes the lowest value of the whole numbers
-    months = round(digits[0]*12)                              #converts the decimal of the year into months
-
-    return [years, months]
-
-
-def tumblr_post(quote_query: list[str]) -> list[str]:
-    
-    quote = random.choice(quote_query)
-    tumblr_url = 'https://www.tumblr.com/search/{}?t=1'
-    search_url = tumblr_url.format(quote.replace(" ", "%20"))
-    html = urllib.request.urlopen(search_url).read()
-    soup = BeautifulSoup(html, 'html.parser')
-
-    a_element = soup.find_all("a", {"class": "BSUG4"})
-    random_a_tag = random.choice(a_element)
     blog_name = random_a_tag.get("data-login-wall-blog-name")
-
+    
     blog_info = client.blog_info(blog_name)
-    total_posts = blog_info['blog']['total_posts']
-    offset = random.randint(0, total_posts)
-    posts_data = client.posts(blog_name, tag=quote, limit=1, offset=offset)
-    random_post_dict_list = posts_data['posts']
-    post_dict = random_post_dict_list[0]
-    blog_post_url = post_dict['post_url']
 
-    if 'text' in post_dict:
-        return [blog_post_url, post_dict['text']]   
+    get_random_post = random.randint(0, blog_info['blog']['total_posts'])
 
-    else:
-        return [blog_post_url, post_dict['trail'][0]['content']]
+    posts_raw_data = client.posts(blog_name,limit=1, offset=get_random_post)
+    post_refined_data = posts_raw_data['posts'][0]
+
+
+    return post_refined_data
             
+def tumblr_text(post_info):
+    try: 
+        return post_info['content'][0]['text']
+    
+    except:
 
-def reddit_picture_url(subreddit_query: list[str]) -> list[str]:
+        try: 
+            return post_info['trail'][0]['content'][0]['text']
+        
+        except:
+            return "default quote"
 
-    sub = random.choice(subreddit_query)
-    subreddit = reddit.subreddit(sub)
-    new_posts = list(subreddit.new(limit=20))
-    random_post = random.choice(new_posts)
-    while not random_post.url.endswith(('.png', '.jpg', '.jpeg')):
-        random_post = subreddit.random()
-    return [sub, random_post.url]
+
+def reddit_picture_url(reddit_subreddit):
+    random_post_id = reddit.subreddit(reddit_subreddit).random()
+    checked = {}
+    while random_post_id.domain != 'i.redd.it':
+        if random_post_id not in checked:
+            checked[random_post_id] = 1
+        
+        random_post_id = reddit.subreddit(reddit_subreddit).random()
+
+    return random_post_id.url
+
+
+def get_message(ann_date):
+    email_message = """\
+            <html>
+                <body>
+                    <p> <em>{tumblr_text}</em> {tumblr_url}  </p>
+                    <br>
+                    <img src = {sub_image}
+                    width="400" height="400"/>
+                </body>
+            </html>
+        """
+    if current_day == ann_date:
+        email_message = """\
+            <html>
+                <h2> Happy {years} years and {months} months!!</h2>
+                <body>
+                    <p> <em>{tumblr_text}</em> {tumblr_url}  </p>
+                    <br>
+                    <img src = {sub_image}
+                    width="400" height="400"/>
+                </body>
+            </html>
+        """
+    return email_message
 
 
 @dataclass
@@ -90,96 +113,48 @@ class Mail:
     to_email: str
     subject: str
     send_time: str
-    ann_year: int
-    ann_month: int
-    ann_day: int
+    ann_date: str
     tumblr_search_query: str
-    subreddit_list: list[str]
+    subreddit_list: "list[str]"
     port: int
     
     def email_depart(self):
-        if today_date == self.ann_day:
-            schedule.every().day.at(self.send_time).do(self.send_ann_mail)
-            
-        else:
-            schedule.every().day.at(self.send_time).do(self.send_mail)
-            
+        self.send_mail()
         while True:
             schedule.run_pending()
             time.sleep(1)
             
     def send_mail(self):
-        tumblr_quote = tumblr_post(self.tumblr_search_query)
-        tumblr_url = tumblr_quote[0]
-        tumblr_text = tumblr_quote[1]
-        reddit_image = reddit_picture_url(self.subreddit_list)
-        reddit_sub = reddit_image[0]
-        sub_image = reddit_image[1]
+        corrected_date = datetime.strptime(self.ann_date, '%Y/%m/%d').date()
+        years = current_year - corrected_date.year
+        months = current_month - corrected_date.month
+        day = corrected_date.day
+        email_html_message = get_message(day)
+
+        tumblr_post_info = tumblr_post(self.tumblr_search_query)
+        tumblr_url = tumblr_post_info['post_url']
+        tumblr_quote = tumblr_text(tumblr_post_info)
+
+        reddit_sub_name = random.choice(self.subreddit_list)
+        reddit_image = reddit_picture_url(reddit_sub_name)
         msg = EmailMessage()
 
         msg["Subject"] = self.subject
         msg["From"] = self.email
         msg["To"] = self.to_email
-        msg.add_alternative("""\
-            <html>
-                <body>
-                    <p> <em>{tumblr_text}</em> {tumblr_url}  </p>
-                    <br>
-                    <img src = {sub_image}
-                    width="400" height="400"/>
-                </body>
-            </html>
-        """.format(tumblr_text=tumblr_text, tumblr_url=tumblr_url, reddit_sub=reddit_sub, sub_image=sub_image), subtype = 'html')
+        msg.add_alternative(email_html_message.format(years=years, months=months,tumblr_text=tumblr_text, tumblr_url=tumblr_url, reddit_sub=reddit_sub, sub_image=sub_image), subtype = 'html')
 
         with smtplib.SMTP_SSL("smtp.gmail.com", self.port) as server:
             server.login(self.email, self.password)
             server.send_message(msg)
             server.quit()
 
-    def send_ann_mail(self):
-        dates = ann_date(self.ann_year, self.ann_month, self.ann_day)
-        years = dates[0]
-        months = dates[1]
-
-        tumblr_quote = tumblr_post(self.tumblr_search_query)
-        tumblr_url = tumblr_quote[0]
-        tumblr_text = tumblr_quote[1]
-
-        reddit_image = reddit_picture_url(self.subreddit_list)
-        reddit_sub = reddit_image[0]
-        sub_image = reddit_image[1]
-        
-        msg = EmailMessage()
-
-        msg["Subject"] = self.subject
-        msg["From"] = self.email
-        msg["To"] = self.to_email
-        msg.add_alternative("""\
-            <html>
-                <h2> Happy {years} years and {months} months!!</h2>
-                <body>
-                    <p> <em>{tumblr_text}</em> {tumblr_url}  </p>
-                    <br>
-                    <img src = {sub_image}
-                    width="400" height="400"/>
-                </body>
-            </html>
-        """.format(years=years, months=months, tumblr_text=tumblr_text, tumblr_url=tumblr_url, reddit_sub=reddit_sub, sub_image=sub_image), subtype = 'html')
-
-        with smtplib.SMTP_SSL("smtp.gmail.com", self.port) as server:
-            server.login(self.email, self.password)
-            server.send_message(msg)
-            server.quit()            
-
-
 email = Mail(email="example@gmail.com",
             password=config_data['EMAIL_PASSWORD'], 
             to_email="receiver@gmail.com", 
             subject="SUBJECT",
             send_time="15:00",
-            ann_year=2020,
-            ann_month=int("08"),
-            ann_day=17, 
+            ann_date="2020/08/02", #YYYY/MM/DD format, 
             tumblr_search_query=["inspirational quotes"], 
             subreddit_list=['frogs', 'CatsWithHats'], 
             port=465)
